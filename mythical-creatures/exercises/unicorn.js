f6fd64ed940e68c5381103f7cab674ab6d9fa1ed
Joshua Everett <59103702+josh-everett01@users.noreply.github.com>
2020-10-11 13:29:58 -0500

    Add creature: UNICORN
    This commit adds the Unicorn to the mythical creatures

--- mythical-creatures/exercises/unicorn.js
+++ mythical-creatures/exercises/unicorn.js
@@ -2,6 +2,19 @@ class Unicorn {
   constructor(name, color) {
     this.name = name;
     this.color = color;
+    if (color === undefined) {
+      this.color = 'white'
+    }
+    this.isWhite = function isWhite(color) {
+      if (color === 'white') {
+        return true
+      } else {
+        return false;
+      }
+    }
+    this.says = function says(phrase) {
+      return `**;* ${phrase} *;**`
+    }
   }
 }
 
